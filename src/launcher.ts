8df1c0d314532459414f949135428876d59d0bdb
Knox <yanbin.huang@chukong-inc.com>
2025-09-16 10:30:38 +0800

    [bug] fix launcher name error (#21)
    * fix launcher name error
    
    * refine code

--- src/launcher.ts
+++ src/launcher.ts
@@ -2,7 +2,6 @@
 import Project from './core/project';
 import Engine from './core/engine';
 import { startupAssetDB } from './core/assets';
-import { join, resolve } from 'path';
 
 class ProjectManager {
     create() {
@@ -34,12 +33,12 @@ class ProjectManager {
     }
 }
 
-
-
 export const projectManager = new ProjectManager();
+
 // 这是测试代码，不能使用单元测试，因为 jest 会捕获 require 然后不走 preload 的特殊处理,导致读不了 cc
 (async () => {
     const { engine } = require('../.user.json');
+    const { join } = require('path');
     const ccEngine = await Engine.init(engine);
     await ccEngine.initEngine({
         importBase: join(engine, 'library'),
